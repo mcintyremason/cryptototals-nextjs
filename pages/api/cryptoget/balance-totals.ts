b1bb5f36328d4e6ec1e5ffdb0a0ee2895b280028
mahun <mcintyre.mlm@gmail.com>
2022-02-12 11:02:50 -0500

    balance-totals

--- pages/api/cryptoget/balance-totals.ts
+++ pages/api/cryptoget/balance-totals.ts
@@ -1,51 +1,39 @@
 import { StatusCodes } from 'http-status-codes'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { camelize, sortObjectByFullName } from '../shared/functions'
-import { CryptoListData, ReducedCryptoListData } from './types/Cryptocompare'
+import { BalanceTotalsResponse } from './types/Cryptocompare'
 const cc = require('cryptocompare')
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  let sortedReducedCryptos = {}
+  const balanceTotalsQuery = _req.query
+  const cryptoNamesArr = []
+  const cryptoBalances: BalanceTotalsResponse = {
+    currencies: {},
+    total: 0,
+  }
 
   try {
-    let cryptoSymbols = {}
-    const reducedCryptos: any[] = []
-    const coinListResponse = await cc.coinList()
-
-    cryptoSymbols = { ...coinListResponse.Data }
-
-    // Reduce original response to fields we care about in the FE app
-    const reduceFields = (cryptoData: CryptoListData): ReducedCryptoListData => {
-      return Object.entries(cryptoData).reduce((acc, [key, value]) => {
-        switch (key) {
-          case 'FullName':
-            return { ...acc, ...{ [camelize(key)]: value } }
-          case 'Symbol':
-            return { ...acc, ...{ [camelize(key)]: value } }
-          case 'TotalCoinsMined':
-            return { ...acc, ...{ [camelize(key)]: value } }
-          default:
-            return acc
-        }
-      }, {}) as ReducedCryptoListData
-    }
-
-    // create object of reduced values for cryptos
-    for (const entry of Object.entries(cryptoSymbols)) {
-      const [_, value] = entry
-      const reducedCrypto = reduceFields(value as CryptoListData)
-
-      if (reducedCrypto?.totalCoinsMined > 10000000) {
-        reducedCryptos.push(reducedCrypto)
+    for (const key of Object.keys(balanceTotalsQuery)) {
+      cryptoNamesArr.push(key)
+      cryptoBalances.currencies = {
+        ...cryptoBalances.currencies,
+        ...{ [key]: { symbol: key, holdings: 0 } },
       }
+      cryptoBalances.currencies[key].holdings = parseFloat(balanceTotalsQuery[key] as string) || 0
     }
 
-    sortedReducedCryptos = sortObjectByFullName(reducedCryptos)
+    const prices = await cc.priceMulti(cryptoNamesArr, 'USD')
+
+    for (const key of Object.keys(prices)) {
+      cryptoBalances.currencies[key].price = prices[key].USD
+      cryptoBalances.currencies[key].total =
+        (cryptoBalances.currencies[key].holdings as number) * prices[key].USD
+      cryptoBalances.total += cryptoBalances.currencies[key].total as number
+    }
   } catch (e) {
     console.error(e)
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(e)
+    return res.status(500).send(e)
   }
-  return res.status(StatusCodes.OK).json(sortedReducedCryptos)
+  return res.status(StatusCodes.OK).json({ ...cryptoBalances })
 }
 
 export default handler
