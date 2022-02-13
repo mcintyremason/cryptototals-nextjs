20159a31636f5859240ea70a726d0f4e3cc456e6
mahun <mcintyre.mlm@gmail.com>
2022-02-13 09:37:05 -0500

    add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,15 +29,15 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          {/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-156934776-1"></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-W2W9CGXN42"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `window.dataLayer = window.dataLayer || [];
-              function gtag() { dataLayer.push(arguments); }
+              function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'UA-156934776-1');`,
+              gtag('config', 'G-W2W9CGXN42');`,
             }}
-          /> */}
+          />
         </body>
       </Html>
     )
