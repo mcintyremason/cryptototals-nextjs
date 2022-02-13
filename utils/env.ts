7366c2ecff7a026ce8ee591d61b33b100f2d962d
mahun <mcintyre.mlm@gmail.com>
2022-02-13 01:44:54 -0500

    relative url

--- utils/env.ts
+++ utils/env.ts
@@ -3,5 +3,5 @@
 export const getCryptogetApiEndpoint = (): string => {
   // built at runtime
   // return env?.REACT_APP_API ? env.REACT_APP_API : 'https://cryptoget.dev/api'
-  return 'http://localhost:3000/api'
+  return '/api'
 }
