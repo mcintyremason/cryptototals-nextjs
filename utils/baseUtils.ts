a997f38f0a75e1c236f97da26464bbc0b4e3b7e6
mahun <mcintyre.mlm@gmail.com>
2022-02-13 09:02:50 -0500

    use baseUtils in api code

--- utils/baseUtils.ts
+++ utils/baseUtils.ts
@@ -1,3 +1,5 @@
+import { ReducedCryptoListData } from '../models/Cryptoget'
+
 export const isEmpty = (value: any): boolean => {
   if (typeof value === 'string') {
     value = value.trim()
@@ -30,4 +32,33 @@ export const stringToColor = (string: string) => {
   }
 
   return color
+}
+
+export const getRandomInt = () => {
+  return Math.floor(Math.random() * 1_000_000_000_000)
+}
+
+export const camelize = (str: string) => {
+  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match: string, index: number) => {
+    if (+match === 0) return '' // or if (/\s+/.test(match)) for white spaces
+    return index === 0 ? match.toLowerCase() : match.toUpperCase()
+  })
+}
+
+export const sortObjectByKeys = (o: any) => {
+  return Object.keys(o)
+    .sort()
+    .reduce((r: any, k) => ((r[k] = o[k]), r), {})
+}
+
+export const sortObjectByFullName = (obj: Array<ReducedCryptoListData>) => {
+  return obj.sort((a: ReducedCryptoListData, b: ReducedCryptoListData) => {
+    if (a.fullName.toLocaleLowerCase() < b.fullName.toLocaleLowerCase()) {
+      return -1
+    }
+    if (a.fullName.toLocaleLowerCase() > b.fullName.toLocaleLowerCase()) {
+      return 1
+    }
+    return 0
+  })
 }
