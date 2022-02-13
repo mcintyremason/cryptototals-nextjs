b2b2321d5304c034c3ae8580c8be6e3f79c99a25
mahun <mcintyre.mlm@gmail.com>
2022-02-13 08:52:44 -0500

    title & description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,21 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <title>CryptoGet</title>
+          <meta charSet="utf-8" />
+          {/* <meta
+        name="google-site-verification"
+        content="yvJk6V5mexkpQDw1vr4H0bE0uIMXrgpJSC9kPx0MDtw"
+      /> */}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name="description"
+            content={`Cryptoget.dev is a website that empowers it's users to see the total value of 
+            their cryptocurrency holdings at a single glance. The total value of a users holdings 
+            is displayed, as well as a breakdown of the value of the holdings for each 
+            individual cryptocurrency.`}
+          />
+          <link rel="icon" type="image/png" href="/img/icons/favicon-1/favicon.ico" />
           <link rel="stylesheet" href="/css/app.css" />
         </Head>
         <body>
